71ad94947dabcde4d83641a4547ffdec0efa1f7d
Happy2018new <chenbo79800@163.com>
2023-06-03 22:55:17 +0800

    :(

--- main.go
+++ main.go
@@ -1,22 +1,56 @@
 package main
 
 import (
-	"encoding/gob"
+	"encoding/json"
 	"fmt"
-	"phoenixbuilder/GameControl/ResourcesControlCenter"
+	"phoenixbuilder/minecraft/protocol/packet"
 )
 
 func main() {
-	a := map[string]any{"2": map[int]any{2: 3}}
-	var b map[string]any
-	gob.Register(map[string]interface{}{})
-	ResourcesControlCenter.DeepCopy(
-		&a,
-		&b,
-		func() {
-			gob.Register(map[string]any{})
-			gob.Register(map[int]any{})
-		},
-	)
-	fmt.Printf("%#v\n", b)
+	var pkt packet.InventoryTransaction
+	m := `{
+		"LegacyRequestID": 0,
+		"LegacySetItemSlots": null,
+		"Actions": [],
+		"TransactionData": {
+			"LegacyRequestID": 0,
+			"LegacySetItemSlots": null,
+			"Actions": null,
+			"ActionType": 0,
+			"BlockPosition": [
+				23,
+				23,
+				23
+			],
+			"BlockFace": 0,
+			"HotBarSlot": 3,
+			"HeldItem": {
+				"StackNetworkID": 0,
+				"Stack": {
+					"NetworkID": 0,
+					"MetadataValue": 0,
+					"BlockRuntimeID": 0,
+					"Count": 0,
+					"NBTData": null,
+					"CanBePlacedOn": null,
+					"CanBreak": null,
+					"HasNetworkID": false
+				}
+			},
+			"Position": [
+				0,
+				0,
+				0
+			],
+			"ClickedPosition": [
+				0,
+				0,
+				0
+			],
+			"BlockRuntimeID": 0
+		}
+	}`
+	err := json.Unmarshal([]byte(m), &pkt)
+	fmt.Printf("%#v\n", pkt)
+	fmt.Println(err)
 }

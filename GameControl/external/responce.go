5344a96772f7d58dacfe2e7661585022ea04bd84
Happy2018new <chenbo79800@163.com>
2023-06-11 11:50:29 +0800

    fix on comment

--- GameControl/external/responce.go
+++ GameControl/external/responce.go
@@ -6,7 +6,7 @@ import "phoenixbuilder/encoding"
 type Responce struct {
 	// 指代此响应包所对应的请求包的回声。
 	// 只有此响应包对应的请求包中提供了此字段时才会存在，
-	// 否则为 null
+	// 否则为空字符串
 	Echo string `json:"request_id"`
 	// 指代对应请求的完成时间
 	FinishTime string `json:"finish_time"`

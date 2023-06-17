0d8bab131964549c9471f5a16ebe04f10535a421
Happy2018new <chenbo79800@163.com>
2023-06-17 11:19:33 +0800

    fix on comments

--- GameControl/GlobalAPI/requestStructure.go
+++ GameControl/GlobalAPI/requestStructure.go
@@ -5,7 +5,7 @@ import (
 	"phoenixbuilder/minecraft/protocol/packet"
 )
 
-// 请求 request 代表的结构请求并获取与之对应的相应体。
+// 请求 request 代表的结构请求并获取与之对应的响应体。
 // 当且仅当租赁服响应结构请求时本函数才会返回值。
 //
 // 请确保在使用此函数前占用了结构资源，否则这将导致程序惊慌
